f77870377c696a8e1830901c2cabbe710064ec63
Andi Tangguh <anditangguh71@yahoo.com>
2018-05-10 20:07:08 +0700

    first push and commits

---
+++ pages/about.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel=stylesheet href="../styles/styles.css">
+        <title> About Me </title>
+    </head>
+
+    <body>
+        <div class="maincontainer">
+            <div class="left">
+                <p>
+                    My name is Andi Tangguh Kippi Nusantara. I am an Informatics college student of Parahyangan Catholic
+                    University, Bandung, Indonesia. I am currently learning about Java and some web designing languages
+                    such as HTML, CSS, and JavaScript. Most of my works are small college projects and tasks and currently
+                    trying to improve my programming skills and quality of my works.
+                </p>
+            </div>
+            
+            <div class="right">
+            
+            </div>
+        </div>
+    </body>
+</html>
